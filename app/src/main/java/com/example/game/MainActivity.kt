83659f69e5ce6b0f5a0eda583bcde92f0793d031
Daloshka <andrey_balakirevv@mail.ru>
2020-06-24 14:59:53 +0800

    second commit

--- app/src/main/java/com/example/game/MainActivity.kt
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("GITTTTT ACTION!", null).show()
+                    .setAction("Haloy ACTION!", null).show()
         }
     }
 
