1cf09a0336efa9f0396278a89bf2550a0d3044f3
Daloshka <andrey_balakirevv@mail.ru>
2020-06-24 19:03:39 +0800

    my first code

--- app/src/main/java/com/example/game/MainActivity.kt
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.example.game
 
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private var textviewFirst:TextView? = null
+    private var cLayout: ConstraintLayout? = null
+    private var number:Byte = 5
+    private var counter:Int = 0
+    private var start:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        textviewFirst = findViewById(R.id.textview_first)
+        textviewFirst?.text = "abc"
+
+        cLayout = findViewById(R.id.cLayout)
+
+        Thread{
+            start = true
+            while (start){
+                Thread.sleep(1000)
+                runOnUiThread(){
+                    if(counter == 5) cLayout?.setBackgroundColor(Color.GRAY)
+                    textviewFirst?.text = getString(counter)
+                    counter++
+                }
+            }
+        }.start()
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -34,5 +60,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        start = false
     }
 }
